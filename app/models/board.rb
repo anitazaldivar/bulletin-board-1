ba8ceaeb6183fa4c705953e95e7075afe12fba85
anitazaldivar <128723674+anitazaldivar@users.noreply.github.com>
2024-05-11 20:29:17 +0000

    final validations ready too

--- app/models/board.rb
+++ app/models/board.rb
@@ -8,6 +8,7 @@
 #  updated_at :datetime         not null
 #
 class Board < ApplicationRecord
+  validates(:name, {presence: true, :uniqueness => true })
   has_many(:posts, class_name: "Post", foreign_key: "board_id")
 
   has_many(:active_posts, -> {   where("expires_on <= ?", Date.today) }, class_name: "Post", foreign_key: "board_id")
